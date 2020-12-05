6c4b7f5a847e51f6c75dd0c0c1f16d7991cdd5f2
Vigneshwar S <vigneshwar.s@msil.ad>
2020-12-05 19:08:45 +0530

    changes

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express')
-const serverless = require('serverless-http')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// })
-
-module.exports = serverless(app)
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
